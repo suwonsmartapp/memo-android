16e7892a1a37dcda71da8cfb5be422385de87311
Junsuk Oh <a811219@gmail.com>
2017-08-21 21:51:30 +0900

    5, 6일차 메모 앱 완성

--- app/src/main/java/com/example/myapplication/MemoDetailActivity.java
+++ app/src/main/java/com/example/myapplication/MemoDetailActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MemoDetailActivity extends AppCompatActivity {
 
@@ -10,6 +12,29 @@ public class MemoDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_detail);
+
+        if (getIntent() != null) {
+            String title = getIntent().getStringExtra("제목");
+            String description = getIntent().getStringExtra("내용");
+
+            ((EditText)findViewById(R.id.title_edit)).setText(title);
+            ((EditText)findViewById(R.id.desc_edit)).setText(description);
+        }
+
+        findViewById(R.id.floatingActionButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = ((EditText)findViewById(R.id.title_edit)).getText().toString();
+                String description = ((EditText)findViewById(R.id.desc_edit)).getText().toString();
+
+                Intent intent = new Intent();
+                intent.putExtra("제목", title);
+                intent.putExtra("내용", description);
+
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/myapplication/MemoListActivity.java
+++ app/src/main/java/com/example/myapplication/MemoListActivity.java
@@ -7,10 +7,10 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.GridView;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,41 +19,90 @@ import java.util.Map;
 
 public class MemoListActivity extends AppCompatActivity {
 
+    // 전체 사물함
+    private List<Map<String, String>> mDataList;
+    private SimpleAdapter mAdapter;
+    private int mPosition = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_list);
 
         // 데이터를 준비
-        List<Map<String, String>> dataList = new ArrayList<>();
+        mDataList = new ArrayList<>();
 
-        // 100개의 임시 데이터
-        for (int i = 0; i < 100; i++) {
-            Map<String, String> data = new HashMap<>();
-            data.put("제목", "제목 " + i);
-            data.put("내용", "어쩌구 저쩌구 " + i);
-            dataList.add(data);
-        }
+//        // 100개의 임시 데이터
+//        for (int i = 0; i < 100; i++) {
+//            Map<String, String> data = new HashMap<>();
+//            data.put("제목", "제목 " + i);
+//            data.put("내용", "어쩌구 저쩌구 " + i);
+//            mDataList.add(data);
+//        }
 
         // 어댑터
-        SimpleAdapter adapter = new SimpleAdapter(this,
-                dataList,
+        mAdapter = new SimpleAdapter(this,
+                mDataList,
                 android.R.layout.simple_list_item_2,
                 new String[] {"제목", "내용"},
                 new int[] { android.R.id.text1, android.R.id.text2});
 
         // 리스트뷰
         ListView listView = (ListView) findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
+        listView.setAdapter(mAdapter);
 
-        // 그리드뷰
-        GridView gridView = (GridView) findViewById(R.id.grid_view);
-        gridView.setAdapter(adapter);
+        // 리스트 클릭
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Map<String, String> data = mDataList.get(position);
 
-        // 스피너
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setAdapter(adapter);
+                Intent intent = new Intent(MemoListActivity.this, MemoDetailActivity.class);
+                intent.putExtra("제목", data.get("제목"));
+                intent.putExtra("내용", data.get("내용"));
 
+                mPosition = position;
+
+                startActivityForResult(intent, 2000);
+            }
+        });
+
+        // +버튼
+        findViewById(R.id.floatingActionButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MemoListActivity.this, MemoDetailActivity.class),
+                        1000);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == 1000 && resultCode == RESULT_OK && intent != null) {
+            String title = intent.getStringExtra("제목");
+            String description = intent.getStringExtra("내용");
+
+            Map<String, String> data = new HashMap<>();
+            data.put("제목", title);
+            data.put("내용", description);
+            mDataList.add(data);
+
+            mAdapter.notifyDataSetChanged();
+        } else if (requestCode == 2000) {
+            String title = intent.getStringExtra("제목");
+            String description = intent.getStringExtra("내용");
+
+            Map<String, String> data = new HashMap<>();
+            data.put("제목", title);
+            data.put("내용", description);
+
+            mDataList.set(mPosition, data);
+
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
     @Override
